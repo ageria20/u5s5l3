186cc20ea1ea9acc8d598f4d26cd31f29ab32600
ageria20 <ageria20@gmail.com>
2024-09-04 14:13:16 +0200

    rename repo

--- src/test/java/ageria/u5s5l3/U5s5l2ApplicationTests.java
+++ src/test/java/ageria/u5s5l3/U5s5l2ApplicationTests.java
@@ -1,13 +1,20 @@
 package ageria.u5s5l3;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class U5s5l2ApplicationTests {
 
+
+
 	@Test
 	void contextLoads() {
 	}
 
+	@BeforeAll
+	static void setUpTesting(){
+
+	}
 }
